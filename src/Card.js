90f098c43372d79932c2818118b69665beceb550
Amisha Vashishtha <66069041+amishavashishtha@users.noreply.github.com>
2020-07-22 01:33:25 +0530

    pushing files

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Card = ({id,name,email})=>{
+ // const {id,name,email} =props;
+	return (
+		<div className='bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt = 'robots' src={`https://robohash.org/${id}?200*200`}/>
+				<div>
+					<h2>{name}</h2>
+					<p>{email}</p>
+				</div>
+	     </div>
+
+		);
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card.js';
+import {Robots} from './Robots.js';
+import 'tachyons'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+	
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<div>
+    <Card id={Robots[0].id} name ={Robots[0].name} email={Robots[0].email}/>
+    <Card id={Robots[1].id} name ={Robots[1].name} email={Robots[1].email}/>
+    <Card id={Robots[2].id} name ={Robots[2].name} email={Robots[2].email}/>
+</div>
+  </React.StrictMode>
+  
+  ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
